6751f9680231c24c5a819c98a0fbda97863c2dff
ov <ovsappro@gmail.com>
2022-02-14 06:11:52 +0000

    3rd commit

---
+++ Day14/moronecho.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "hello world"
+// input new line, tabs etc.
+echo -e "First tab \t 2nd tab \t 3rd tab"
+echo "----------------------------------------"
+echo -e "First\t\tSecond\t\tThird \n Amar \t\t Akbar \t\t Antony"
+
