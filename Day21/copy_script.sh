b487a84fa6f043096fa5663deae9e3b6bd2decbf
ov <ovsappro@gmail.com>
2022-02-22 19:23:58 +0000

    Commit after 3rd iteration , covered logging, getopts with case, functions

--- Day21/copy_script.sh
+++ Day21/copy_script.sh
@@ -1,22 +1,38 @@
-#!/bin/bash
+#!/bin/bash 
 
 #
-#function to teach usage
+#Logging function
 #
-function usage() {
-echo "Usage: $0 MOUNT [MOUNTN]..." >&2
+function logger() {
+	if [[ ${VERBOSE} == "TRUE"  ]]
+	then
+		echo "${@}" >> ${0}_$(date +"%d%m%y_%T").log 
+		${@}	
+		#this line prints it to the output as well, making it verbose
+	else
+		echo "${@}" >> ${0}_$(date +"%d%m%y_%T").log
+	fi
 }
 
 #
-#function to copy
+#function to teach Usage
 #
+function usage() {
+logger echo "Usage: $0 [-vs] MOUNT [MOUNTN]..."  
+logger echo "	-v Increase verbosity."
+exit 1
+}
+
+#
+#function to copy, here the mounts are stored in a temporary file called 'file_mounts'
+#this function was skipped in the second iteration
 function copy_themount() {
 
 	LINENUM=1
 	while read LINE
 	do
 		VAR="$(echo $LINE | awk -F " " '{print $NF}')"
-		echo "${VAR}_$(date +%d_%m_%y)"
+		logger echo "${VAR}_$(date +%d_%m_%y)"
 		#echo "$VAR_$(command)" did not print the $VAR value hence use ${VAR}
 		# here you can probably add the copy command
 		((LINENUM++))
@@ -27,6 +43,7 @@ function copy_themount() {
 #
 #function to create the file with information on the mounts to be copied
 #this function takes an argument which is user defined, and it is the SID-->First Iteration
+#this function was also skipped in the second iteration
 function create_file_mounts2copy() {
 	
 	ID=$1
@@ -35,15 +52,16 @@ function create_file_mounts2copy() {
 	df -kh | grep -i $ID 1> file_mounts
 
 }
+#
 #this function takes the argument with Script --> SecondIteration
-
+#here copyscrpt.sh [-vs] /dev/shm is passed
 function copy_themount2() {
 	MNTLOC=${1}
-	echo "from insdie the function --> ${MNTLOC}"
+	logger echo "from insdie the function --> ${MNTLOC}"
 }
 
 
-#check free space
+#check free space, if free space is more than Zero percent then return 1
 function spacecheck() {
 	MNTLOC=${1}
 	SPACE=$(df -kh ${MNTLOC} | awk -F " " '{print $((NF-1))}' | tail -1 | head -c1)
@@ -62,17 +80,47 @@ function spacecheck() {
 #give id as dev in pluralsight
 #read -p "enter the ID - " X
 
+#
+#-----------------------------------Below is 3rd Iteration -- added -v -s options--------------------
+#
+while getopts vs OPTION
+do
+	case ${OPTION} in 
+		v)
+			VERBOSE="TRUE" 
+			ARG=${OPTIND}
+			ARG1=$((ARG-1))
+			shift $ARG1
+			echo "ARG is ${ARG} and ARG1 is ${@}"
+			# this is the only way to get the remaining arguments
+			#learn this shift command and all that
+			#if v had an argument to be added eg: v: & 10 then $OPTARG = 10
+			;;
+		s)
+			SILENT="TRUE"
+			;;
+		?)
+			echo "Invalid Option"
+			usage 
+			# you have to be careful, there is no () after function call
+			;;
+	esac
+done
+
+#---------------------------------end of 3rd Iteration------------------------------------------
+#
+
 #below section is when we give the mount values while running the script
 #added it in the second iteration
 #-------------------------------------------------2nd Iteration-----------------------
 #check if the script is run with proper inputs in case of no read statement is present.
-#yu can create a USAGE function
+#yu can create a USAGE function, check if there are no parameters added
 if [ -z "${1}" ]
 	#be careful about the SPACE in the above if expression
 then
 	usage
 else
-	echo "${@} will be copied"
+	logger echo "${@} will be copied"
 fi
 
 
@@ -82,14 +130,17 @@ MNTLIST=${@}
 
 for i in ${MNTLIST}
 do
-	echo "from the main function ==> ${i}"
+	logger echo "from the main function ==> ${i}"
 	#check if there is enough space on the mount
 	spacecheck ${i}
+#	logger echo "return status --> ${?}"
+# only the previous command status is checked so the if loop should be immediately after the function call
+#return status check
 	if [ ${?} -eq 0 ]
 	then
 		copy_themount2 ${i}
 	else
-		echo "the space @ ${i} is not Zero"
+		logger echo "the space @ ${i} is not Zero"
 	fi
 
 done
